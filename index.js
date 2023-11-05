6853ad37aecf074e050763fbff969c78d0064f49
rajat jana <rajatjana2024@gmail.com>
2023-11-05 13:08:41 +0530

    using express and handling routes

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const http = require("http");
-const fs = require("fs");
-var url = require('url');
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
 const PORT = 8000;
 
-const myServer = http.createServer((req, res) => {
-  console.log("new req recived");
 
-  //url parsing
-  const myUrl = url.parse(req.url,true);
-  console.log(myUrl.query);
-  
-  var log = `\n${Date.now()} ${req.url}`;
-  fs.appendFile("./log.txt", log, () => {
-    switch (myUrl.pathname) {
-      case "/":
-        res.end("welcome to home\n"+myUrl.query.name+"  "+myUrl.query.id);
-        break;
+function updatLog(req,res){
+  var log = `\n${Date.now()} ${req.method} ${req.path} ${req.url}`;
+  fs.appendFile('./log.txt',log,(err)=>{});
+}
 
-      case "/about":
-        res.end("i am rajat");
-        break;
-
-      case "/results":
-       res.end(`your search rersults for ${myUrl.query.search_query} are >>>`);
-
-      default:
-        res.end("it 404 error\n"+myUrl.query.name+"  "+myUrl.query.id);
-        break;
-    }
-  });
+app
+.route('/')
+.get((req,res)=>{
+  console.log(`new ${req.method} request received`);
+  updatLog(req,res);
+  return res.json({status:"sucess"});
+})
+.put((req,res)=>{
+  console.log(`new ${req.method} request received`);
+  updatLog(req,res);
+  return res.json({status:"sucess"});
+})
+.patch((req,res)=>{
+  console.log(`new ${req.method} request received`);
+  updatLog(req,res);
+  return res.json({status:"sucess"});
+})
+.delete((req,res)=>{
+  console.log(`new ${req.method} request received`);
+  updatLog(req,res);
+  return res.json({status:"sucess"});
+})
+.post((req,res)=>{
+  console.log(`new ${req.method} request received`);
+  updatLog(req,res);
+  return res.json({status:"sucess"});
 });
 
-myServer.listen(PORT, () => console.log(`server started at PORT=${PORT}`));
+
+app.listen(PORT,()=>console.log("server connected"));
